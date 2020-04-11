a536c4dc7d1352f87a55efcd8ccd47d8dfaef2fe
Ahmed Al-Salih <ahmed.al-salih@delhaize.com>
2020-04-11 13:14:45 -0400

    get cell sample data

--- Classification/SUPPORT VECTOR MACHINES (SVMS)/svm.py
+++ Classification/SUPPORT VECTOR MACHINES (SVMS)/svm.py
@@ -7,3 +7,12 @@ Created on Fri Apr 10 12:49:37 2020
 Support Vector Machine (or SVM),
 """
 
+
+import pandas as pd
+import pylab as pl
+import numpy as np
+import scipy.optimize as opt
+from sklearn import preprocessing
+from sklearn.cross_validation import train_test_split
+# %matplotlib inline 
+import matplotlib.pyplot as plt
