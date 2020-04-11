7796955e2c07b762fa2e11af569ae9e324dd1879
Ahmed Al-Salih <ahmed.al-salih@delhaize.com>
2020-04-11 14:00:18 -0400

    SVM lab

---
+++ Classification/SUPPORT VECTOR MACHINES (SVMS)/README.md
@@ -0,0 +1,7 @@
+
+
+#### cross_validation to model_selection
+renaming and deprecation of cross_validation sub-module to model_selection. 
+change cross_validation to model_selection:
+
+py: from sklearn.model_selection import train_test_split
--- Classification/SUPPORT VECTOR MACHINES (SVMS)/svm.py
+++ Classification/SUPPORT VECTOR MACHINES (SVMS)/svm.py
@@ -13,6 +13,133 @@ import pylab as pl
 import numpy as np
 import scipy.optimize as opt
 from sklearn import preprocessing
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 # %matplotlib inline 
 import matplotlib.pyplot as plt
+
+"""Load the Cancer data
+The example is based on a dataset that is publicly available from the UCI Machine Learning Repository 
+(Asuncion and Newman, 2007)[http://mlearn.ics.uci.edu/MLRepository.html]. The dataset consists of several
+ hundred human cell sample records, each of which contains the values of a set of cell characteristics."""
+#Click here and press Shift+Enter or use !wget to download it from IBM Object Storage.
+# !wget -O cell_samples.csv https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/ML0101ENv3/labs/cell_samples.csv
+
+cell_df = pd.read_csv("cell_samples.csv")
+cell_df.head()
+
+"""The ID field contains the patient identifiers. The characteristics of the cell samples from each patient are contained in fields Clump to Mit. The values are graded from 1 to 10, with 1 being the closest to benign.
+
+The Class field contains the diagnosis, as confirmed by separate medical procedures, as to whether the samples are benign (value = 2) or malignant (value = 4).
+
+Lets look at the distribution of the classes based on Clump thickness and Uniformity of cell size:"""
+
+ax = cell_df[cell_df['Class'] == 4][0:50].plot(kind='scatter', x='Clump', y='UnifSize', color='DarkBlue', label='malignant');
+cell_df[cell_df['Class'] == 2][0:50].plot(kind='scatter', x='Clump', y='UnifSize', color='Yellow', label='benign', ax=ax);
+plt.show()
+
+## Data pre-processing and selection
+cell_df.dtypes
+# It looks like the BareNuc column includes some values that are not numerical. We can drop those rows:
+cell_df = cell_df[pd.to_numeric(cell_df['BareNuc'], errors='coerce').notnull()]
+cell_df['BareNuc'] = cell_df['BareNuc'].astype('int')
+cell_df.dtypes
+
+
+feature_df = cell_df[['Clump', 'UnifSize', 'UnifShape', 'MargAdh', 'SingEpiSize', 'BareNuc', 'BlandChrom', 'NormNucl', 'Mit']]
+X = np.asarray(feature_df)
+X[0:5]
+
+"""We want the model to predict the value of Class (that is, benign (=2) or malignant (=4)). 
+As this field can have one of only two possible values, we need to change its measurement level to reflect this."""
+
+cell_df['Class'] = cell_df['Class'].astype('int')
+y = np.asarray(cell_df['Class'])
+y [0:5]
+
+## Train/Test dataset
+X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.2, random_state=4)
+print ('Train set:', X_train.shape,  y_train.shape)
+print ('Test set:', X_test.shape,  y_test.shape)
+
+from sklearn import svm
+clf = svm.SVC(kernel='rbf')
+clf.fit(X_train, y_train) 
+# After being fitted, the model can then be used to predict new values:
+
+yhat = clf.predict(X_test)
+yhat [0:5]
+
+## Evaluation
+
+from sklearn.metrics import classification_report, confusion_matrix
+import itertools
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+
+# Compute confusion matrix
+cnf_matrix = confusion_matrix(y_test, yhat, labels=[2,4])
+np.set_printoptions(precision=2)
+
+print (classification_report(y_test, yhat))
+
+# Plot non-normalized confusion matrix
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=['Benign(2)','Malignant(4)'],normalize= False,  title='Confusion matrix')
+
+# You can also easily use the __f1_score__ fron sklearn library:
+    
+from sklearn.metrics import f1_score
+f1_score(y_test, yhat, average='weighted') 
+
+# try jaccard index for accuracy:
+from sklearn.metrics import jaccard_similarity_score
+jaccard_similarity_score(y_test, yhat)
+
+
+"""Practice
+Can you rebuild the model, but this time with a linear kernel? You can use __ kernel='linear'__ option, 
+when you define the svm. How the accuracy changes with the new kernel function?"""
+
+
+
+
+
+
+
+
+
+
+
